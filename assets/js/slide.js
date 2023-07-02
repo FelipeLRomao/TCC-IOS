fef826f7cd8f5ee0e526a615823b63f91807133e
FelipeLRomao <feliperomaodev@gmail.com>
2023-07-02 11:36:27 -0300

    Adicionando novo design site

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,15 +1,15 @@
-let count = 1;
-document.getElementById('slide1').checked = true;
+// let count = 1;
+// document.getElementById('slide1').checked = true;
 
-setInterval( function(){
-    nextImage();
-}, 5000)
+// setInterval( function(){
+//     nextImage();
+// }, 2000)
 
-function nextImage(){
-    count++;
-    if(count>3){
-        count = 1;
-    }
+// function nextImage(){
+//     count++;
+//     if(count>3){
+//         count = 1;
+//     }
 
-    document.getElementById('slide'+count).checked = true;
-}
+//     document.getElementById('slide'+count).checked = true;
+// }
